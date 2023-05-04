043e9ab660cbc4c8c37c168668bb159b9b58c3b7
Thomas Bouillon <thomas.bouillon@orange.com>
2023-05-04 10:59:11 +0200

    meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,26 @@ import Dogs from "@/components/Dogs";
 import Hero from "@/components/Hero";
 import KennelPresentation from "@/components/KennelPresentation";
 import StevePresentation from "@/components/StevePresentation";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>L&apos;ecrin de baloo</title>
+                <meta
+                    name="description"
+                    content="Découvrez pour vos vacances aux Coches, les balades en chien de traîneau l'hiver et les balades avec les chiens en été !"
+                />
+                <meta name="image" content="/images/cani-rando.jpg" />
+                <meta property="og:title" content="L'ecrin de baloo" />
+                <meta
+                    property="og:description"
+                    content="Découvrez pour vos vacances aux Coches, les balades en chien de traîneau l'hiver et les balades avec les chiens en été !"
+                />
+                <meta property="og:image" content="/images/cani-rando.jpg" />
+                <meta property="og:locale" content="fr_FR" />
+            </Head>
             <Hero />
             <main className="bg-white">
                 <Activities />
